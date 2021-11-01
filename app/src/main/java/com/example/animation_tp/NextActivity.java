670e952990b07b34b7b1b635ceb4bb55ad7c66d3
Qyzei <yahya.jaafae@gmail.com>
2021-11-01 19:06:19 +0100

    yahya

---
+++ app/src/main/java/com/example/animation_tp/NextActivity.java
@@ -0,0 +1,17 @@
+package com.example.animation_tp;
+
+import android.view.animation.Animation;
+
+import com.example.animation_tp.R;
+
+public class NextActivity {
+    Animation animation = NextActivity.loadAnimation(getApplicationContext(), R.anim.myanimation);
+
+    private static Animation loadAnimation(Object applicationContext, int myanimation) {
+        return null;
+    }
+
+    private Object getApplicationContext() {
+        return null;
+    }
+}
